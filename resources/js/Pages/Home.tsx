515027b67019c001365b6a206178f92609e28053
Rogério Portela da Luz <64877260+RogerioPortela94@users.noreply.github.com>
2025-11-08 07:49:10 -0300

    Teste inicial

---
+++ resources/js/Pages/Home.tsx
@@ -0,0 +1,9 @@
+export default function Home() {
+    return (
+      <div style={{ textAlign: 'center', marginTop: '3rem' }}>
+        <h1>🚀 Laravel + React + Inertia + TypeScript</h1>
+        <p>Configuração completa e funcionando!</p>
+      </div>
+    )
+  }
+  
