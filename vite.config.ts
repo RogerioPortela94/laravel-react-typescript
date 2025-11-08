948930e7da20edf943ebaf89a09e8897c396478c
Rogério Portela da Luz <64877260+RogerioPortela94@users.noreply.github.com>
2025-11-08 07:38:10 -0300

    intalação typescript e  react

--- vite.config.ts
+++ vite.config.ts
@@ -3,9 +3,10 @@ import laravel from 'laravel-vite-plugin';
 import tailwindcss from '@tailwindcss/vite';
 
 export default defineConfig({
+    
     plugins: [
         laravel({
-            input: ['resources/css/app.css', 'resources/js/app.js'],
+            input: ['resources/css/app.css', 'resources/js/app.tsx'],
             refresh: true,
         }),
         tailwindcss(),
